ad5a72dfba37b3827efcc6ae04ed9e4995a33b0f
Tom Kitchen <tomkitchen@Toms-MBP.lan>
2020-05-19 12:47:11 +0100

    install yum list and update path

--- run.sh
+++ run.sh
@@ -4,13 +4,8 @@ SCRIPTROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir -p build/python/lib/python3.7/site-packages build/bin
 BUILDDIR="$(cd $SCRIPTROOT/../build && pwd)"
 
-if [[ -s $SCRIPTROOT/pip ]];then
-  cd $BUILDDIR
-  pip3.7 install -r $SCRIPTROOT/pip -t python/lib/python3.7/site-packages
-#  zip -r $SCRIPTROOT/pip.zip .
-fi
-
 if [[ -s $SCRIPTROOT/yum ]];then
+  yum install $(cat yum) -y
   cd /tmp
   yumdownloader $(echo n | sudo yum install $(cat $SCRIPTROOT/yum) | grep "will be installed" | cut -d " " -f 3 | paste -sd " ")
   rpmdev-extract *rpm
@@ -19,6 +14,14 @@ if [[ -s $SCRIPTROOT/yum ]];then
   cp -R /tmp/*/usr/lib64/* .
   cd $BUILDDIR/bin
   cp $(find /tmp/*/usr/libexec -type f) .
+fi
+
+PATHS_ARRAY=($BUILDDIR/lib64/*/bin)
+export PATH=$PATH:$(echo ${PATHS_ARRAY[@]}: | tr " " :)
+if [[ -s $SCRIPTROOT/pip ]];then
+  cd $BUILDDIR
+  pip3.7 install -r $SCRIPTROOT/pip -t python/lib/python3.7/site-packages
+#  zip -r $SCRIPTROOT/pip.zip .
 fi
 
 cd $BUILDDIR
